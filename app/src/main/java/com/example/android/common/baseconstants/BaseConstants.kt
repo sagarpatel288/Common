4157082ab2343e7b1c6e2af9f75fe77b71681d2a
srdpatel <sagarpatel288@gmail.com>
2020-02-29 10:30:11 +0530

    14.0 Add flexibility to change base url at run time in ServiceGenerator class

--- app/src/main/java/com/example/android/common/baseconstants/BaseConstants.kt
+++ app/src/main/java/com/example/android/common/baseconstants/BaseConstants.kt
@@ -1,5 +1,13 @@
 package com.example.android.common.baseconstants
 
 
-const val BASE_API : String = "";
+/**
+ * 2/29/2020
+ * If you want to change [BASE_URL], make it kind of static using companion object
+ * @see <a href="http://google.com"></a>
+ * [ReadableHyperlinkText]( "")
+ * @author srdpatel
+ * @since 1.0
+ */
+const val BASE_URL : String = "https://api.github.com/";
 const val PARCEL: String = "parcel"
---
+++ app/src/main/java/com/example/android/common/baserest/ServiceGenerator.kt
@@ -0,0 +1,63 @@
+package com.example.android.common.baserest
+
+import com.example.android.common.BuildConfig
+import com.example.android.common.baseconstants.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+/**
+ * 2/29/2020
+ * Adaptive, flexible and sustainable retrofit client class for network api calls
+ * <p>
+ * All class members are static here because we don't want to eat up memory by creating new objects each and every time
+ * to use unnecessary new open socket connection. We need only one to reuse open socket connections.
+ * </p>
+ * {@link #} []
+ *
+ * @author srdpatel
+ * @see <a href="https://futurestud.io/tutorials/retrofit-2-creating-a-sustainable-android-client">Source</a>
+ * [Source] (https://futurestud.io/tutorials/retrofit-2-creating-a-sustainable-android-client "Source")
+ * @since 1.0
+ */
+class ServiceGenerator {
+
+    // comment by srdpatel: 2/29/2020 companion object acts like a static methods but it can be only inside the class
+    companion object {
+
+        var baseUrl: String = BASE_URL
+        private var retrofitBuilder =
+            Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(baseUrl)
+        private var okHttpClientBuilder = OkHttpClient.Builder()
+        private val httpLoggingInterceptor = HttpLoggingInterceptor()
+        private var retrofit =
+            retrofitBuilder
+                .client(getOkHttpClient())
+                .build()
+
+        @JvmStatic
+        fun getOkHttpClient(): OkHttpClient {
+            if (BuildConfig.DEBUG && !okHttpClientBuilder.interceptors().contains(httpLoggingInterceptor)) {
+                httpLoggingInterceptor.level = (HttpLoggingInterceptor.Level.BODY)
+                okHttpClientBuilder.addInterceptor(httpLoggingInterceptor)
+            }
+            return okHttpClientBuilder.build()
+        }
+
+        @JvmStatic
+        fun <S> withBaseUrl(baseUrl: String, apiInterface: Class<S>): S {
+            retrofitBuilder =
+                Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(baseUrl).client(getOkHttpClient())
+
+            retrofit = retrofitBuilder.build()
+
+            return retrofit.create(apiInterface)
+        }
+    }
+}
