4f52253738a6465bc5431637699b45ffcf4c7c91
srdpatel <sagarpatel288@gmail.com>
2020-03-08 17:44:43 +0530

    20.0 Add AuthInterceptor for networking package

---
+++ app/src/main/java/com/example/android/common/networking/AuthInterceptor.kt
@@ -0,0 +1,17 @@
+package com.github.harmittaa.koinexample.networking
+
+import com.github.harmittaa.koinexample.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor() : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var req = chain.request()
+        // DON'T INCLUDE API KEYS IN YOUR SOURCE CODE
+        // Edit (or add) a gradle.properties file in your project root
+        // and add the API_KEY there!
+        val url = req.url.newBuilder().addQueryParameter("APPID", BuildConfig.API_KEY).build()
+        req = req.newBuilder().url(url).build()
+        return chain.proceed(req)
+    }
+}
