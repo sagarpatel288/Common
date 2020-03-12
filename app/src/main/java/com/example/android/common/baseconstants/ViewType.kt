d4acb017d1c5a84b62d566e04817ec9ecaa24e83
srdpatel <sagarpatel288@gmail.com>
2020-03-12 12:06:56 +0530

    31.0 push #30.0

--- app/src/main/java/com/example/android/common/baseconstants/ViewType.kt
+++ app/src/main/java/com/example/android/common/baseconstants/ViewType.kt
@@ -1,4 +1,22 @@
 package com.example.android.common.baseconstants
 
-enum class ViewType {
+/**
+ * 3/12/2020
+ * Taken sealed class over enum class considering its usage in when clause (freedom from else)
+ * <p>
+ *
+ * </p>
+ *  {@link #} []
+ *
+ * @param
+ * @return
+ * @author srdpatel
+ * @see <a href="http://google.com"></a>
+ * [] (http://google.com "")
+ * @since 1.0
+ */
+sealed class ViewType(viewType: Int) {
+    object ViewTypeItem : ViewType(100)
+    object ViewTypeLoading : ViewType(110)
+    object ViewTypeShimmer : ViewType(120)
 }
