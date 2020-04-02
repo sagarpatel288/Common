dc07d093c9068dc4954f61ba7ccd448e8f39cc22
srdpatel <sagarpatel288@gmail.com>
2020-04-03 01:37:38 +0530

    47.0 Push for commit #46.0

--- app/src/main/java/com/example/android/common/basemodels/BasePojo.kt
+++ app/src/main/java/com/example/android/common/basemodels/BasePojo.kt
@@ -1,4 +1,8 @@
 package com.example.android.common.basemodels
 
-class BasePojo {
-}
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+// comment by srdpatel: 4/3/2020 Need to check if given default values are working or not 
+@Parcelize
+data class BasePojo (var isSelected: Boolean = true, var isClickable: Boolean = true) : Parcelable
