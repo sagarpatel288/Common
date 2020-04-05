84da30bfd1d0c247052b4a2961aeb410301a0b74
srdpatel <sagarpatel288@gmail.com>
2020-04-06 03:08:46 +0530

    50.0 Push for #49.0 (Your working tree contains more)

--- app/src/main/java/com/example/android/common/basemodels/BaseUserInfo.kt
+++ app/src/main/java/com/example/android/common/basemodels/BaseUserInfo.kt
@@ -1,4 +1,26 @@
 package com.example.android.common.basemodels
 
-class BaseUserInfo {
-}
+/**
+ * 4/4/2020
+ * This class considers common properties that every user object would have
+ * <p>
+ *
+ * </p>
+ *  {@link #} []
+ *
+ * @param
+ * @return
+ * @author srdpatel
+ * @see <a href="http://google.com"></a>
+ * [] (http://google.com "")
+ * @since 1.0
+ */
+data class BaseUserInfo @JvmOverloads constructor(
+    var userId: String = "",
+    var loginId: String = "",
+    var defaultName: String = "",
+    var email: String = "",
+    var mobile: String = "",
+    var countryCode: String = "",
+    var photo: String = ""
+)
