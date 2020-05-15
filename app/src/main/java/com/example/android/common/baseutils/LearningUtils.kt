e462093ecb95ab41af74ad2ef790d0794bfa340f
srdpatel <sagarpatel288@gmail.com>
2020-05-15 10:33:23 +0530

    feat: Add more examples of scope functions in LearningUtils Commit: 69.0

--- app/src/main/java/com/example/android/common/baseutils/LearningUtils.kt
+++ app/src/main/java/com/example/android/common/baseutils/LearningUtils.kt
@@ -1,6 +1,7 @@
 package com.example.android.common.baseutils
 
 import com.example.android.common.basemodels.BaseUserInfo
+import java.util.*
 
 class LearningUtils : (Int, Int) -> Int {
 
@@ -31,10 +32,24 @@ class LearningUtils : (Int, Int) -> Int {
         myFunction(a, b) // OR: ```myFunction.invoke(a, b)```
     }
 
-    private fun doSomething(a: Int, b: Int, ft: (Int, Int) -> Int): String {
-        val result = ft(a, b)
-        return "doSomething: $result"
+    //region A higher order function that has one and only function type parameter
+    private fun doSomething(ft: (Int, Int) -> Int) {
+        val result = ft(1, 2)
+        /*...*/
     }
+    //endregion
+
+    //region A function type that has only one parameter
+    private fun doSomethingX(ft: (Int) -> String) {
+        /*...*/
+    }
+    //endregion
+
+    //region A function type that has no parameter
+    private fun doSomethingY(ft: () -> String) {
+        /*...*/
+    }
+    //endregion
 
     private fun percentage(a: Int, b: Int): Int {
         return ((a * b) * 100) / 100
@@ -99,11 +114,35 @@ class LearningUtils : (Int, Int) -> Int {
 
         //region Function literal
         // comment by srdpatel: 4/22/2020 Function literal stored in a variable
-        val sum = { a: Int, b: Int -> a + b }
+        val sum = { a: Int, b: Int ->
+            println()
+            a + b
+        }
 
         // comment by srdpatel: 4/22/2020 How to use function literal
         println("function literal with arguments: " + sum.invoke(1, 2))
         println("calling function literal stored in a variable: " + sum(1, 2))
+
+        /**
+         * While calling the higher order function When the function type parameter is one and only parameter,
+         * and if the function type has also one and only parameter,
+         * in addition to function call parentheses, we can also omit the function type parameter and an arrow ```->```
+         * We just write core business logic between curly braces
+         * and we can access our single parameter through the keyword it like below:
+         */
+        doSomethingX { "$it" }
+
+        /**
+         * 5/8/2020
+         * A function type without any parameter
+         * <p>
+         * if the higher order function has only one function type parameter and the function type has no parameter,
+         * We can simply write our business logic between curly parentheses right after the name of the function.
+         * </p>
+         * @author srdpatel
+         * @since 1.0
+         */
+        doSomethingY { "test" }
 
         /**
          * 4/21/2020
@@ -125,6 +164,33 @@ class LearningUtils : (Int, Int) -> Int {
             println("printing from function literal")
             a + b
         }
+
+        /**
+         * 5/7/2020
+         * Last statement is considered as return statement for a function literal
+         * <p>
+         * Below method will give an error:
+         * ```
+         * Type mismatch.
+         * Required: Int
+         * found: Unit
+         * ```
+         * Because, the last statement of a function literal we have passed as an argument,
+         * makes the return type: [Unit] whereas the higher order function [doSomethingReturnUnit]
+         * expects the function type argument to return [Int]
+         * </p>
+         * @author srdpatel
+         * @since 1.0
+         */
+//        doSomethingReturnUnit(3,4){a: Int, b: Int -> println()}
+
+        /**
+         * While calling the higher order function When the function type parameter is only parameter,
+         * we can simply write our business logic between curly braces after writing the function name
+         * and omit function call parentheses like below:
+         */
+        doSomething { x: Int, y: Int -> x + y }
+
         //endregion
 
         //region Lambda expression
@@ -200,7 +266,6 @@ class LearningUtils : (Int, Int) -> Int {
             fun(a: Int, b: Int): Int = a + b / 2)
         //endregion
 
-
         //region Extension function
         /**
          * 4/30/2020
@@ -212,13 +277,14 @@ class LearningUtils : (Int, Int) -> Int {
          * In order to create an extension function,
          * we write the class name followed by a dot, followed by our function.
          * The class name followed by a dot is called a receiver or receiver type.
-         * In this example, receiver is: String
+         * In this example, receiver type is: String
          * </p>
          *
          * @author srdpatel
          * @since 1.0
          */
         fun String.extensionFunction() {
+            toUpperCase(Locale.ENGLISH)
             //We can access the receiver (Here, it is String) using "this" keyword inside the block
             println("from extension function: $this")
         }
@@ -246,21 +312,210 @@ class LearningUtils : (Int, Int) -> Int {
 
         //region Extension function type parameter in higher order function
         fun doSomething(x: Int, y: Int, ft: Int.(Int) -> Int) {
-            val ftWithReceiver = x.ft(y) //OR ft(x, y). Both will do x-y
+            val ftWithReceiver = x.ft(y) //OR ft(x, y) where x is considered as a receiver.
             println("doSomething: $ftWithReceiver")
         }
 
+        //A lambda with receiver type. Accessing the receiver by using the keyword: this
         val ftSubtraction: Int.(Int) -> Int = { y: Int -> this - y }
 
-        //passing as an anonymous function
+        //passing as a function literal. Accessing the receiver by using the keyword: this
         doSomething(1, 2) { y: Int -> this - y } //prints: doSomething: -1
 
-        //passing as a lambda variable
+        //passing a lambda for the higher order function
         doSomething(1, 2, ftSubtraction) //prints: doSomething: -1
 
 
         //endregion
 
+
+        //endregion
+
+        //region Same receiver type for both an extension function and its function type param
+        fun Int.doSomething(y: Int, ft: Int.(Int) -> Int) {
+            //In such case, we can omit the receiver while calling function type
+            val ftWithReceiver = ft(y)
+            println("doSomething: $ftWithReceiver")
+        }
+
+        //passing as an anonymous function
+        1.doSomething(2) { y: Int -> this - y } //prints: doSomething: -1
+
+        //passing a lambda for the higher order function
+        1.doSomething(2, ftSubtraction) //prints: doSomething: -1
+        //endregion
+
+        //region Scope functions
+        val name = "test"
+        val sb: StringBuilder? = StringBuilder("test")
+
+        fun doSomething(name: String) {
+            println("Name from the method doSomething is: $name")
+        }
+
+        //region with, demonstrating how it is useful for multiple operations
+        fun withDemo() {
+            with(name) {
+                println("the original name is: $this")
+                //Some random operations on the receiver type
+                reversed()
+                toUpperCase(Locale.ENGLISH)
+                //Sending original receiver type.
+                doSomething(this)
+            }
+        }
+        //endregion
+
+        //region let, demonstrating how we can avoid variable
+        StringBuilder("test").let {
+            //it refers to a StringBuilder here
+            println("the original name is: $it")
+            it.reversed()
+            it.toString()
+//            println(it) //It makes the return type unit and the next chain call of any function will be on Unit
+        }.let {
+            //it refers to a String type here
+            println("first to second let block: $it")
+            it.toUpperCase(Locale.ENGLISH)
+        }.let {
+            //it is a String type with all upper case letters here
+            println("second to third let bloc: $it")
+            doSomething(it) //[doSomething] is a function that accepts a string argument
+        }
+        //endregion
+
+        fun equivalentOfLet() {
+            println("the original name is: $sb")
+            sb?.reversed()
+            val ns = sb?.toString()
+            println("first to second let block: $ns")
+            val nsUc = ns?.toUpperCase(Locale.ENGLISH)
+            println("second to third let block: $nsUc")
+            doSomething(nsUc ?: "") //[doSomething] is a function that accepts a string argument
+        }
+
+        //region let, demonstrating how it is useful for nullable variable
+        sb?.let {
+            //it refers to a StringBuilder here
+            println("the original name is: $it")
+            it.reversed()
+            it.toString()
+//            println(it) //It makes the return type unit and the next chain call of any function will be on Unit
+        }?.let {
+            //it refers to a String type here
+            println("first to second let block: $it")
+            it.toUpperCase(Locale.ENGLISH)
+        }?.let {
+            //it is a String type with all upper case letters here
+            println("second to third let bloc: $it")
+            doSomething(it) //[doSomething] is a function that accepts a string argument
+        }
+        //endregion
+
+        //region run, demonstrating how it is useful for nullable variable
+        sb?.run {
+            //it refers to a StringBuilder here
+            println("the original name is: $this")
+            reversed()
+            toString()
+//            println(it) //It makes the return type unit and the next chain call of any function will be on Unit
+        }?.run {
+            //it refers to a String type here
+            println("first to second let block: $this")
+            toUpperCase(Locale.ENGLISH)
+        }?.run {
+            //it is a String type with all upper case letters here
+            println("second to third let bloc: $this")
+            doSomething(this) //[doSomething] is a function that accepts a string argument
+        }
+        //endregion
+
+        //region let + T.run + also
+        sb?.also {
+            //it refers to a StringBuilder here
+            println("the original name is: $it")
+        }?.run {
+            reversed()
+            toString()
+//            println(it) //It makes the return type unit and the next chain call of any function will be on Unit
+        }?.also {
+            //it refers to a String type here
+            println("first to second let block: $it")
+        }?.run {
+            toUpperCase(Locale.ENGLISH)
+        }?.also {
+            //it is a String type with all upper case letters here
+            println("second to third let block: $it")
+        }?.let {
+            doSomething(it) //[doSomething] is a function that accepts a string argument
+        } ?: run {
+            println("string is null")
+        }
+        //endregion
+
+        //region compare let + run in Elvis
+        fun letRun() {
+            var random: Int? = null
+            //Take the variable global when you test
+            random?.let {
+                println("Random number is not null...")
+                println(it)
+                println("Gave random number")
+            }
+                ?: kotlin.run {
+                    println("Generating random number...")
+                    random = (1..100).random()
+                    println("Generated number is: $random")
+                }
+        }
+
+        fun withoutLetRun() {
+            //Take the variable global when you test
+            var random: Int? = null
+            if (random != null) {
+                println("Random number is not null...")
+                println(random)
+                println("Gave random number")
+            } else {
+                println("Generating random number...")
+                random = (1..100).random()
+                println("Generated number is: $random")
+            }
+        }
+
+        //endregion
+
+
+        //region else part will not be executed
+        fun computeElements(): List<Int>? = emptyList()
+
+        fun letDoesNothing() {
+            val maxElement = computeElements()?.let { it.max() } ?: return
+            // This will print nothing...
+            println("Max element was $maxElement")
+        }
+
+        fun compareLet() {
+            val list: List<Int>? = computeElements()
+            fun newTest() {
+                val maxElement = if (list != null) list.max() else return
+                // This will print...
+                println("Max element was $maxElement")
+            }
+        }
+        //endregion
+
+        //region with
+        fun withLetDemo(){
+            with(name) {
+                println("the original name is: $this")
+                reversed()
+                toUpperCase(Locale.ENGLISH)
+            }.let{
+                doSomething(it)
+            }
+        }
+        //endregion
 
         //endregion
     }
@@ -284,8 +539,30 @@ class LearningUtils : (Int, Int) -> Int {
         return p1 + p2
     }
 
-
     fun BaseUserInfo.myExtensionFunction(): String {
         return this.userId + " " + this.defaultName
     }
+
+    //region Inline function
+    inline fun Int.doSomething(y: Int, noinline ftOne: Int.(Int) -> Int, ftTwo: (Int) -> Int) {
+        val funOne =
+            someFunction(ftOne) //passing a function type to another function within higher order function body
+        val funTwo = ftTwo(y)//executing / calling [ftTwo]
+        /*...*/
+    }
+
+    //region Compiler suggestion to not use [inline] keyword if the only lambda parameter is [noinline]
+    inline fun Int.extensionFun(y: Int, noinline ftOne: Int.(Int) -> Int) {
+        val funOne =
+            someFunction(ftOne) //passing a function type to another function within higher order function body
+        /*...*/
+    }
+    //endregion
+
+    fun someFunction(ft: Int.(Int) -> Int): Int {
+        val result = 2.ft(1)
+        println("from some function: $result")
+        return result
+    }
+    //endregion
 }
